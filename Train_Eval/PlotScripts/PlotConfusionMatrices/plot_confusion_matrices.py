5935b8ee8e647e378a571cfcf05c304be3a54d4c
​​Hernán Andrés Morales-Navarrete <75286199+hernanmorales-navarrete@users.noreply.github.com>
2022-12-29 04:51:58 +0100

    Add files via upload
    Add filter for calculation of f-score and accuracy only on  selected classes

--- Train_Eval/PlotScripts/PlotConfusionMatrices/plot_confusion_matrices.py
+++ Train_Eval/PlotScripts/PlotConfusionMatrices/plot_confusion_matrices.py
@@ -37,8 +37,13 @@ def plot_confusion_matrices(output_folder,
     EC.collect_detections(with_severity=with_severities)
     class_names = EC.map_class_list(class_names,with_severities,classes_with_severities,severities)
 
-
-    #old_measure confusion matrix
+    # Filter only fro selected classes
+    #print("class_names : ", class_names)
+    #print("EC.human_labels before:", len(EC.human_labels))
+    EC.human_labels, EC.algotightm_labels = filter_labels(EC.human_labels, EC.algotightm_labels, class_names)
+    #print("EC.human_labels after :", len(EC.human_labels))
+    
+    #measure confusion matrix
     cm_normal = confusion_matrix(EC.human_labels, EC.algotightm_labels, labels = class_names)
 
     _, newMatrix = EC.plot_confusion_matrix(cm_normal, class_names)
@@ -49,7 +54,10 @@ def plot_confusion_matrices(output_folder,
                      yticklabels=class_names,                     
                      vmin=0, vmax=1,
                      cbar=0,
-                     annot=not disable_numbers, cmap = "viridis" )
+                     annot=not disable_numbers,
+                     annot_kws={"size": 18},
+                     fmt="0.2f",
+                     cmap = "viridis" )
     print("name : ", os.path.splitext(os.path.basename(cfg_filename))[0])
     cfg_filename = os.path.splitext(os.path.basename(cfg_filename))[0]
 #    if (not name):
@@ -70,10 +78,14 @@ def plot_confusion_matrices(output_folder,
     with open(path_to_confusion_bins, "w", newline='') as fp:
         writer = csv.writer(fp, delimiter=",")
         writer.writerows(csv_confusion_bin_list)
+        
+    print("CM : ", cm_normal)
+    print("CM sum: ", sum(sum(cm_normal)))
+        
     #-------------------------------------------------------------
     #f1_score
     f1_score_ = f1_score(EC.human_labels, EC.algotightm_labels, labels=class_names, average=None)
-    print(f1_score_)
+    #print("f1_score : ", f1_score_)
 
     path_to_f1_score_csv = os.path.join(output_folder, cfg_filename + "_f1_score.csv")
     with open(path_to_f1_score_csv, "w", newline='') as fp:
@@ -92,8 +104,25 @@ def plot_confusion_matrices(output_folder,
     # -------------------------------------------------------------
     #accuracy_score
     accuracyscore = accuracy_score(EC.human_labels, EC.algotightm_labels, normalize=True)
-    print(accuracyscore)
+    print("accuracy_score: ", accuracyscore)
+    accuracyscore1 = accuracy_score(EC.human_labels, EC.algotightm_labels, normalize=False)
+    print("accuracy_score: ", accuracyscore1)  
     path_to_accuracy_csv = os.path.join(output_folder, cfg_filename + "_accuracy.txt")
     with open(path_to_accuracy_csv, "w", newline='') as fp:
         writer = csv.writer(fp, delimiter=",")
         writer.writerow([accuracyscore])
+    
+    print("TP recalculated : ", accuracyscore*sum(sum(cm_normal)))    
+        
+def filter_labels(human_labels, algorithm_labels, class_names):
+    new_human_labels =[]
+    new_algorithm_labels=[]
+    for i in range(0, len(human_labels)):
+        #if ((human_labels[i] != "CUT") and (algorithm_labels[i] != "CUT") and (human_labels[i] != "NODAL_25") and (algorithm_labels[i] != "NODAL_25") and (human_labels[i] != "BMP_25") and (algorithm_labels[i] != "BMP_25")):
+        if ((human_labels[i] in class_names) and (algorithm_labels[i] in class_names)):
+        #if (algorithm_labels[i] in class_names):
+            new_human_labels.append(human_labels[i])
+            new_algorithm_labels.append(algorithm_labels[i])
+
+    return new_human_labels, new_algorithm_labels 
+           
